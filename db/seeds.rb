abd2fe662be6b48fde697ade0f355bcd707ecc1f
mathieu jelsch <mathieu.jelsch@gmail.com>
2023-05-23 21:23:50 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  faker_title = Faker::Lorem.sentence(word_count: 4)
+  faker_content = Faker::Lorem.sentence(word_count: 12)
+  Article.create(title: faker_title, content: faker_content)
+end
